bead05b4a91d7e2bd0f8cb21a508730593288cd4
yulitarvc <127048937+yulitarvc@users.noreply.github.com>
2023-04-17 16:07:31 +0700

    Add files via upload

---
+++ Praktek 6/Praktikum 1/penulisanArray.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<title> Pemrograman PHP dengan Array </title>
+</head>
+<body>
+<?php
+//penulisan array dapat dibuat seperti berikut
+$nama[] = "Agung Teguh";
+$nama[] = "Wibowo";
+$nama[] = "Almais";
+echo $nama[1] . $nama[2] . $nama[0];
+echo "<br>";
+?>
+</body>
+</html>
